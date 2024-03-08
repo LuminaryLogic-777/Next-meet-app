06604c1ca3105ae3a8f620eb4aaa2a4de6ed07bb
sonaiClayMind <sonaiClayMind@gmail.com>
2024-03-08 18:05:17 +0530

    added audio as media and peer created for create & join

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-// import "@/styles/globals.css";
+import "@/styles/globals.css";
+import { Suspense } from 'react';
 import { SocketProvider } from "@/context/socket";
 
 export default function App({ Component, pageProps }) {
   return (
-    <SocketProvider>
-      <Component {...pageProps} />
-    </SocketProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SocketProvider>
+        <Component {...pageProps} />
+      </SocketProvider>
+    </Suspense>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,56 @@
-import { useSocket } from "@/context/socket";
-import { useEffect } from "react";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/router";
+
 
 export default function Home() {
-  const socket=useSocket();
+  const router = useRouter();
+  const [roomId, setRoomId] = useState("");
 
-  useEffect(()=>{
-    socket?.on("connect",()=>{
-      console.log("id",socket.id);
-    })
-  },[socket]);
-  
+  const joinRoom = () => {
+    if (roomId) {
+      router.push(`/${roomId}`);
+    } else {
+      alert("Please enter a valid room ID.");
+    }
+  };
+
+  const createAndJoin = () => {
+    const roomId = uuidv4();
+    router.push(`/${roomId}`);
+  };
+
   return (
-    <h>Welcome</h>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">
+          Welcome to Next Meet
+        </h1>
+        <div className="mb-6">
+          <input
+            type="text"
+            placeholder="Enter room ID"
+            className="border border-gray-300 px-4 py-2 rounded-lg w-full focus:outline-none focus:border-blue-500"
+            value={roomId}
+            onChange={(e) => setRoomId(e?.target?.value)}
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-lg w-full mt-4 transition-colors duration-300 ease-in-out"
+            onClick={joinRoom}
+          >
+            Join room
+          </button>
+        </div>
+        <div className="flex items-center justify-center text-gray-500 mb-4">
+          OR
+        </div>
+        <button
+          onClick={createAndJoin}
+          className="bg-green-500 hover:bg-green-600 text-white px-6 py-2 rounded-lg w-full transition-colors duration-300 ease-in-out"
+        >
+          Create room
+        </button>
+      </div>
+    </div>
   );
 }
